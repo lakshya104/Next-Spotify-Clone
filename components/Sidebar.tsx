9acb7ba0248c0ae812ade13f753d052a85e7b893
lakshya104 <lakshya.vyas@anktech.co.in>
2023-12-04 17:09:24 +0530

    Initial UI Done

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -2,8 +2,11 @@
 
 import { usePathname } from "next/navigation";
 import { useMemo } from "react";
-import { HiHome} from "react-icons/hi";
+import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
+import Box from "./Box";
+import SidebarItem from "./SidebarItem";
+import Library from "./Library";
 
 interface SidebarProps {
   children: React.ReactNode;
@@ -30,7 +33,25 @@ const Sidebar: React.FC<SidebarProps> = ({ children }) => {
     [pathname]
   );
 
-  return <div>{children}</div>;
+  return (
+    <div className="flex h-full">
+      <div className="hidden md:flex flex-col space-y-2 bg-black h-full w-[300px] p-2">
+        <Box>
+          <div className="flex flex-col space-y-4 px-5 py-4">
+            {routes.map((item) => (
+              <SidebarItem key={item.label} {...item} />
+            ))}
+          </div>
+        </Box>
+        <Box className="overflow-y-auto h-full">
+          <Library/>
+        </Box>
+      </div>
+      <main className="h-full flex-1 overflow-y-auto">
+        {children}
+      </main>
+    </div>
+  );
 };
 
 export default Sidebar;
